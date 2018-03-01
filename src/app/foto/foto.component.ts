236afab48f9c4d567ed0ebba866d9584f0dfa80d
rqguzman <rafael.guzman@outlook.com>
2018-03-01 11:27:26 -0300

    Initial commit

---
+++ src/app/foto/foto.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'foto',
+    templateUrl: './foto.component.html'
+})
+export class FotoComponent {
+
+}
---
+++ src/app/foto/foto.module.ts
@@ -0,0 +1,11 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { FotoComponent } from './foto.component';
+
+@NgModule({
+  imports: [ BrowserModule ],
+  declarations: [ FotoComponent ],
+  exports: [ FotoComponent ]
+})
+export class FotoModule {}
